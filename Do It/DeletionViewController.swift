e4addf891653275f6a21fca39bca31b6dda51291
Wyzzard123 <32074198+Wyzzard123@users.noreply.github.com>
2017-10-29 10:18:49 +0800

    just before the deletion view controller

---
+++ Do It/DeletionViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DeletionViewController.swift
+//  Do It
+//
+//  Created by Wyz on 29/10/17.
+//  Copyright © 2017 Wyz. All rights reserved.
+//
+
+import UIKit
+
+class DeletionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
